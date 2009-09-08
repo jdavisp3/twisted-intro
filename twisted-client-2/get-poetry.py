87c4d33c41ad7eb2c22da34527744df65e111fe4
Dave Peticolas <dave@krondo.com>
2009-09-07 20:41:33 -0700

    Simplify.

--- twisted-client-2/get-poetry.py
+++ twisted-client-2/get-poetry.py
@@ -60,26 +60,21 @@ class PoetryProtocol(Protocol):
         print  msg % (self.task_num, len(data), self.transport.getHost())
 
     def connectionLost(self, reason):
-        def report():
-            print 'Task %d: %d bytes of poetry' % (self.task_num,
-                                                   len(self.poem))
-        from twisted.internet import reactor
-        reactor.addSystemEventTrigger('during', 'shutdown', report)
         self.poemReceived()
 
     def poemReceived(self):
-        self.factory.poem_finished()
+        self.factory.poem_finished(self.task_num, self.poem)
 
 
 class PoetryClientFactory(ClientFactory):
 
     task_num = 1
 
-    protocol = PoetryProtocol
+    protocol = PoetryProtocol # tell base class what proto to build
 
     def __init__(self, poetry_count):
         self.poetry_count = poetry_count
-        self.poems = [] # (task num, poem)
+        self.poems = {} # task num -> poem
 
     def buildProtocol(self, address):
         proto = ClientFactory.buildProtocol(self, address)
@@ -87,12 +82,20 @@ class PoetryClientFactory(ClientFactory):
         self.task_num += 1
         return proto
 
-    def poem_finished(self):
+    def poem_finished(self, task_num=None, poem=None):
+        if task_num is not None:
+            self.poems[task_num] = poem
+
         self.poetry_count -= 1
 
         if self.poetry_count == 0:
+            self.report()
             from twisted.internet import reactor
             reactor.stop()
+
+    def report(self):
+        for i in self.poems:
+            print 'Task %d: %d bytes of poetry' % (i, len(self.poems[i]))
 
     def clientConnectionFailed(self, connector, reason):
         print 'Failed to connect to:', connector.getDestination()
