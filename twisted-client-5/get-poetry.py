99c466aba504ddf3b7f10b658b164ac3676e9d88
Dave Peticolas <dave@krondo.com>
2009-12-06 10:07:19 -0800

    Client 5.0.

---
+++ twisted-client-5/get-poetry.py
@@ -0,0 +1,159 @@
+# This is the Twisted Get Poetry Now! client, version 5.0
+
+import optparse, random, sys
+
+from twisted.internet import defer
+from twisted.internet.protocol import Protocol, ClientFactory
+
+
+def parse_args():
+    usage = """usage: %prog [options] [hostname]:port ...
+
+This is the Get Poetry Now! client, Twisted version 5.0
+Run it like this:
+
+  python get-poetry.py port1 port2 port3 ...
+
+If you are in the base directory of the twisted-intro package,
+you could run it like this:
+
+  python twisted-client-5/get-poetry.py 10001 10002 10003
+
+to grab poetry from servers on ports 10001, 10002, and 10003.
+
+Of course, there need to be servers listening on those ports
+for that to work.
+"""
+
+    parser = optparse.OptionParser(usage)
+
+    _, addresses = parser.parse_args()
+
+    if not addresses:
+        print parser.format_help()
+        parser.exit()
+
+    def parse_address(addr):
+        if ':' not in addr:
+            host = ''
+            port = addr
+        else:
+            host, port = addr.split(':', 1)
+
+        if not port.isdigit():
+            parser.error('Ports must be integers.')
+
+        return host, int(port)
+
+    return map(parse_address, addresses)
+
+
+class PoetryProtocol(Protocol):
+
+    poem = ''
+
+    def dataReceived(self, data):
+        self.poem += data
+
+    def connectionLost(self, reason):
+        self.poemReceived(self.poem)
+
+    def poemReceived(self, poem):
+        self.factory.poem_finished(poem)
+
+
+class PoetryClientFactory(ClientFactory):
+
+    protocol = PoetryProtocol
+
+    def __init__(self, deferred):
+        self.deferred = deferred
+
+    def poem_finished(self, poem):
+        if self.deferred is not None:
+            d, self.deferred = self.deferred, None
+            d.callback(poem)
+
+    def clientConnectionFailed(self, connector, reason):
+        if self.deferred is not None:
+            d, self.deferred = self.deferred, None
+            d.errback(reason)
+
+
+def get_poetry(host, port):
+    """
+    Download a poem from the given host and port. This function
+    returns a Deferred which will be fired with the complete text of
+    the poem or a Failure if the poem could not be downloaded.
+    """
+    d = defer.Deferred()
+    from twisted.internet import reactor
+    factory = PoetryClientFactory(d)
+    reactor.connectTCP(host, port, factory)
+    return d
+
+
+class GibberishError(Exception): pass
+
+def cummingsify(poem):
+    """
+    Randomly do one of the following:
+
+    1. Return a cummingsified version of the poem.
+    2. Raise a GibberishError.
+    3. Raise a CannotCummingsify error.
+    """
+
+    def success():
+        return poem.lower()
+
+    def gibberish():
+        raise GibberishError()
+
+    def bug():
+        raise ValueError()
+
+    return random.choice([success, gibberish, bug])()
+
+
+def poetry_main():
+    addresses = parse_args()
+
+    from twisted.internet import reactor
+
+    poems = []
+    errors = []
+
+    def try_to_cummingsify(poem):
+        try:
+            return cummingsify(poem)
+        except GibberishError:
+            raise
+        except:
+            print 'Cummingsify failed!'
+            return poem
+
+    def got_poem(poem):
+        print poem
+        poems.append(poem)
+
+    def poem_failed(err):
+        print >>sys.stderr, 'The poem download failed.'
+        errors.append(err)
+
+    def poem_done(_):
+        if len(poems) + len(errors) == len(addresses):
+            reactor.stop()
+
+    for address in addresses:
+        host, port = address
+        d = get_poetry(host, port)
+        d.addCallback(try_to_cummingsify)
+        d.addCallbacks(got_poem, poem_failed)
+        d.addBoth(poem_done)
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    poetry_main()
