c25c00850c3e546e6cae97659c6b99f60e4495d1
Dave Peticolas <dave@krondo.com>
2009-12-29 11:29:28 -0800

    Twisted server 1.0.

---
+++ twisted-server-1/fastpoetry.py
@@ -0,0 +1,78 @@
+# This is the Twisted Fast Poetry Server, version 1.0
+
+import optparse, os
+
+from twisted.internet.protocol import Factory, Protocol
+
+
+def parse_args():
+    usage = """usage: %prog [options] poetry-file
+
+This is the Fast Poetry Server, Twisted edition.
+Run it like this:
+
+  python fastpoetry.py <path-to-poetry-file>
+
+If you are in the base directory of the twisted-intro package,
+you could run it like this:
+
+  python twisted-server-1/fastpoetry.py poetry/ecstasy.txt
+
+to serve up John Donne's Ecstasy, which I know you want to do.
+"""
+
+    parser = optparse.OptionParser(usage)
+
+    help = "The port to listen on. Default to a random available port."
+    parser.add_option('--port', type='int', help=help)
+
+    help = "The interface to listen on. Default is localhost."
+    parser.add_option('--iface', help=help, default='localhost')
+
+    options, args = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('Provide exactly one poetry file.')
+
+    poetry_file = args[0]
+
+    if not os.path.exists(args[0]):
+        parser.error('No such file: %s' % poetry_file)
+
+    return options, poetry_file
+
+
+class PoetryProtocol(Protocol):
+
+    def connectionMade(self):
+        self.transport.write(self.factory.poem)
+        self.transport.loseConnection()
+
+
+class PoetryFactory(Factory):
+
+    protocol = PoetryProtocol
+
+    def __init__(self, poem):
+        self.poem = poem
+
+
+def main():
+    options, poetry_file = parse_args()
+
+    poem = open(poetry_file).read()
+
+    factory = PoetryFactory(poem)
+
+    from twisted.internet import reactor
+
+    port = reactor.listenTCP(options.port or 0, factory,
+                             interface=options.iface)
+
+    print 'Serving %s on %s.' % (poetry_file, port.getHost())
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
