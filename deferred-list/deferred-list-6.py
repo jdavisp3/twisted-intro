1cb5ed4fff320bdbaf045887ea26e0f3ed37aa6a
Dave Peticolas <dave@krondo.com>
2010-08-18 23:00:44 -0700

    DeferredList examples.

---
+++ deferred-list/deferred-list-6.py
@@ -0,0 +1,13 @@
+from twisted.internet import defer
+
+def got_results(res):
+    print 'We got:', res
+
+d1 = defer.Deferred()
+d2 = defer.Deferred()
+d = defer.DeferredList([d1, d2])
+d.addCallback(got_results)
+print 'Firing d1.'
+d1.callback('d1 result')
+print 'Firing d2 with errback.'
+d2.errback(Exception('d2 failure'))
