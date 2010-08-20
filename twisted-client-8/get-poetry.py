ba7c4d17d0cf0eb076407070d2412897e5527df6
Dave Peticolas <dave@krondo.com>
2010-08-19 21:03:21 -0700

    Client 8.0.

---
+++ twisted-client-8/get-poetry.py
@@ -0,0 +1,192 @@
+# This is the Twisted Get Poetry Now! client, version 8.0
+
+import optparse, sys
+
+from twisted.internet import defer
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.protocols.basic import NetstringReceiver
+
+
+def parse_args():
+    usage = """usage: %prog [options] [hostname]:port ...
+
+This is the Get Poetry Now! client, Twisted version 8.0
+Run it like this:
+
+  python get-poetry.py xform-port port1 port2 ...
+
+If you are in the base directory of the twisted-intro package,
+you could run it like this:
+
+  python twisted-client-6/get-poetry.py 10001 10002 10003
+
+to grab poetry from servers on ports 10002, and 10003 and transform
+it using the server on port 10001.
+
+Of course, there need to be appropriate servers listening on those
+ports for that to work.
+"""
+
+    parser = optparse.OptionParser(usage)
+
+    _, addresses = parser.parse_args()
+
+    if len(addresses) < 2:
+        print parser.format_help()
+        parser.exit()
+
+    def parse_address(addr):
+        if ':' not in addr:
+            host = ''
+            port = addr
+        else:
+            host, port = addr.split(':', 1)
+
+        if not port.isdigit():
+            parser.error('Ports must be integers.')
+
+        return host, int(port)
+
+    return map(parse_address, addresses)
+
+
+class PoetryProtocol(Protocol):
+
+    poem = ''
+
+    def dataReceived(self, data):
+        self.poem += data
+
+    def connectionLost(self, reason):
+        self.poemReceived(self.poem)
+
+    def poemReceived(self, poem):
+        self.factory.poem_finished(poem)
+
+
+class PoetryClientFactory(ClientFactory):
+
+    protocol = PoetryProtocol
+
+    def __init__(self, deferred):
+        self.deferred = deferred
+
+    def poem_finished(self, poem):
+        if self.deferred is not None:
+            d, self.deferred = self.deferred, None
+            d.callback(poem)
+
+    def clientConnectionFailed(self, connector, reason):
+        if self.deferred is not None:
+            d, self.deferred = self.deferred, None
+            d.errback(reason)
+
+
+class TransformClientProtocol(NetstringReceiver):
+
+    def connectionMade(self):
+        self.sendRequest(self.factory.xform_name, self.factory.poem)
+
+    def sendRequest(self, xform_name, poem):
+        self.sendString(xform_name + '.' + poem)
+
+    def stringReceived(self, s):
+        self.transport.loseConnection()
+        self.poemReceived(s)
+
+    def poemReceived(self, poem):
+        self.factory.handlePoem(poem)
+
+
+class TransformClientFactory(ClientFactory):
+
+    protocol = TransformClientProtocol
+
+    def __init__(self, xform_name, poem):
+        self.xform_name = xform_name
+        self.poem = poem
+        self.deferred = defer.Deferred()
+
+    def handlePoem(self, poem):
+        d, self.deferred = self.deferred, None
+        d.callback(poem)
+
+    def clientConnectionLost(self, _, reason):
+        if self.deferred is not None:
+            d, self.deferred = self.deferred, None
+            d.errback(reason)
+
+    clientConnectionFailed = clientConnectionLost
+
+
+class TransformProxy(object):
+    """
+    I proxy requests to a transformation service.
+    """
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def xform(self, xform_name, poem):
+        factory = TransformClientFactory(xform_name, poem)
+        from twisted.internet import reactor
+        reactor.connectTCP(self.host, self.port, factory)
+        return factory.deferred
+
+
+def get_poetry(host, port):
+    """
+    Download a poem from the given host and port. This function
+    returns a Deferred which will be fired with the complete text of
+    the poem or a Failure if the poem could not be downloaded.
+    """
+    d = defer.Deferred()
+    from twisted.internet import reactor
+    factory = PoetryClientFactory(d)
+    reactor.connectTCP(host, port, factory)
+    return d
+
+
+def poetry_main():
+    addresses = parse_args()
+
+    xform_addr = addresses.pop(0)
+
+    proxy = TransformProxy(*xform_addr)
+
+    from twisted.internet import reactor
+
+    @defer.inlineCallbacks
+    def get_transformed_poem(host, port):
+        try:
+            poem = yield get_poetry(host, port)
+        except Exception, e:
+            print >>sys.stderr, 'The poem download failed:', e
+            raise
+
+        try:
+            poem = yield proxy.xform('cummingsify', poem)
+        except Exception:
+            print >>sys.stderr, 'Cummingsify failed!'
+
+        defer.returnValue(poem)
+
+    def got_poem(poem):
+        print poem
+
+    ds = []
+
+    for (host, port) in addresses:
+        d = get_transformed_poem(host, port)
+        d.addCallbacks(got_poem)
+        ds.append(d)
+
+    dlist = defer.DeferredList(ds, consumeErrors=True)
+    dlist.addCallback(lambda res : reactor.stop())
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    poetry_main()
