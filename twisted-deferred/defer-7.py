65c307bed371c6fc341f287ac6384f98f510da4e
Dave Peticolas <dave@krondo.com>
2010-08-31 19:52:50 -0700

    Simplify examples.

--- twisted-deferred/defer-7.py
+++ twisted-deferred/defer-7.py
@@ -1,7 +1,7 @@
 from twisted.internet.defer import Deferred
 def out(s): print s
 d = Deferred()
-d.addCallbacks(lambda r: out(r), lambda e: out(e))
+d.addCallbacks(out, out)
 d.errback(Exception('First error'))
 d.callback('First result')
 print 'Finished'
