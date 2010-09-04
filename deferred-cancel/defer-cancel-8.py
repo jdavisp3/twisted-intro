020852cd27e3396a4230cd704c6160aca5f0138d
Dave Peticolas <dave@krondo.com>
2010-09-04 00:14:34 -0700

    Cancel examples.

---
+++ deferred-cancel/defer-cancel-8.py
@@ -0,0 +1,16 @@
+from twisted.internet import defer
+
+def canceller(d):
+    print "I need to cancel this deferred:", d
+
+def callback(res):
+    print 'callback got:', res
+
+def errback(err):
+    print 'errback got:', err
+
+d = defer.Deferred(canceller) # created by lower-level code
+d.addCallbacks(callback, errback) # added by higher-level code
+d.callback('result')
+d.cancel()
+print 'done'
---
+++ deferred-cancel/defer-cancel-9.py
@@ -0,0 +1,28 @@
+from twisted.internet.defer import Deferred
+
+def send_poem(d):
+    print 'Sending poem'
+    d.callback('Once upon a midnight dreary')
+
+def get_poem():
+    """Return a poem 5 seconds later."""
+    from twisted.internet import reactor
+    d = Deferred()
+    reactor.callLater(5, send_poem, d)
+    return d
+
+def got_poem(poem):
+    print 'I got a poem:', poem
+
+def poem_error(err):
+    print 'get_poem failed:', err
+
+def main():
+    from twisted.internet import reactor
+    reactor.callLater(10, reactor.stop) # stop the reactor in 10 seconds
+
+    d = get_poem()
+    d.addCallbacks(got_poem, poem_error)
+    reactor.run()
+
+main()
