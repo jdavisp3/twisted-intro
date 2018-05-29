cd4c7a886af759d56f0916cf13287a4eda43637d
Karthik Desai <karthik.desai@iav.de>
2018-05-29 11:45:13 +0200

    Catch error on Windows as polling is not supported

--- basic-twisted/simple-poll.py
+++ basic-twisted/simple-poll.py
@@ -1,5 +1,9 @@
-from twisted.internet import pollreactor
-pollreactor.install()
+import platform
+if platform.system() == 'Windows':
+    print("Pollreactor not supported")
+else:
+    from twisted.internet import pollreactor
+    pollreactor.install()
 
 from twisted.internet import reactor
 reactor.run()
